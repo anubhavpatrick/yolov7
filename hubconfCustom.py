c0937c560eb7ff8c3d0a141ad9283845137c9069
anubhavpatrick <anubhavpatrick@gmail.com>
2023-02-23 14:40:39 +0530

    Commented and refactored hubconfCustom.py

--- hubconfCustom.py
+++ hubconfCustom.py
@@ -14,17 +14,16 @@ import time
 import cv2
 import torch
 import numpy as np
-from numpy import random
 
 from models.experimental import attempt_load
 from utils.general import check_img_size, non_max_suppression, scale_coords, set_logging
 from utils.plots import plot_one_box
-from utils.torch_utils import select_device, time_synchronized
+from utils.torch_utils import select_device
 
 #send mail alert
 from send_mail import prepare_and_send_email
 
-#Global Variables
+#Global Variabless
 is_email_allowed = False #when user checks the email checkbox, this variable will be set to True
 send_next_email = True #We have to wait for 10 minutes before sending another email
 # NEXT TWO STATEMENTS NEED TO BE CHANGED TO MATCH YOUR SETUP!!!
@@ -48,7 +47,7 @@ opt  = {
 }
 
 
-def violation_alert_generator(im0, subject='PPE Violation Detected', message_text='A PPE violation is detected at ABESIT'):
+def violation_alert_generator(im0, subject='PPE Violation Detected', message_text='A PPE violation is detected'):
     '''This function will send an email with attached alert image and then wait for 10 minutes before sending another email
     
     Parameters:
@@ -131,181 +130,145 @@ def video_detection(conf_=0.25, frames_buffer=[]):
   global send_next_email
   global is_email_allowed
   global email_recipient
-
-  violation_frames = 0 # Number of frames with violation
-  
-  import time
-  #start_time = time.time()
-  # total_detections = 0
-
-  # create a list to store the detections
   global detections_summary
 
-  #------ Customization  made by Anubhav Patrick------#
+  violation_frames = 0 # Number of frames with violation
  
   #pop first frame from frames_buffer to get the first frame
+  # We encountered a bug in which the first frame was not getting properly processed so we are popping it out
   while True:
     if len(frames_buffer) > 0:
       _ = frames_buffer.pop(0)
       break
-    #frame = frames_buffer.pop(0)
 
-  #else path_x is a video file
-  else:
-    video_path = path_x
-    video = cv2.VideoCapture(video_path)
-    _, _ = video.read()
-
-    #Video information
-    fps = video.get(cv2.CAP_PROP_FPS)
-    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    nframes = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    print("Video information: ")
-    print("FPS: ", fps)
-    print("Width: ", w)
-    print("Height: ", h)
-    print("Number of frames: ", nframes)
-
-  # Initialzing object for writing video output
-  # output = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'DIVX'),fps , (w,h))
+  # empty the GPU cache to free up memory for inference
   torch.cuda.empty_cache()
   # Initializing model and setting it for inference
+  # no_grad() is used to speed up inference by disabling gradient calculation
   with torch.no_grad():
     weights, imgsz = opt['weights'], opt['img-size']
     set_logging()
     device = select_device(opt['device'])
-    half = device.type != 'cpu'
     model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    
+    # if device is not cpu i.e, it is gpu, convert model to half precision
+    half = device.type != 'cpu'
     if half:
-      model.half()
+      model.half() # convert model to FP16
 
+    # find names of classes in the model
     names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    
+    # Run inference one time to initialize the model on a tensor of zeros
     if device.type != 'cpu':
       model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))
 
+    # classes to filter out from the detections
+    # We will not be filtering out any class and use all four - safe, unsafe, no-helmet and no-jacket
     classes = None
     if opt['classes']:
       classes = []
       for class_name in opt['classes']:
         classes.append(opt['classes'].index(class_name))
 
-    skip_frame = False
-    #for j in range(nframes):
-    while True:
-    
-        #------- Customization made by Anubhav Patrick --------#
-        #if is_stream:
-          # check if there are frames in the buffer
-
+    try:
+      # Contously run inference on the frames in the buffer
+      while True:
+        # if the frames_buffer is not empty, pop the first frame from the buffer
         if len(frames_buffer) > 0:
           #pop first frame from frames_buffer 
           img0 = frames_buffer.pop(0)
+          # if the popped frame is None, continue to the next iteration
           if img0 is None:
             continue
-          #print("Dimensions of frame: ", img0.shape)
           ret = True #we have successfully read one frame from stream
+          #clear the buffer if it has more than 10 frames to avoid memory overflow
           if len(frames_buffer) >= 10:
-            frames_buffer.clear() #clear the buffer if it has more than 10 frames to avoid memory overflow
+            frames_buffer.clear() 
         else:
           # buffer is empty, nothing to do
           continue
+
+        # Resize and pad image while meeting stride-multiple constraints
+        img = letterbox(img0, imgsz, stride=stride)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
+        img = np.ascontiguousarray(img) # convert to contiguous array
+        img = torch.from_numpy(img).to(device) # place the image on the device (cpu or gpu)
+        img = img.half() if half else img.float()  # convert to FP16 if device is gpu
+        img /= 255.0  # Normalize to [0, 1] range
         
-        '''else:
-          # do predictions on alternate frames
-          if skip_frame:
-            ret, img0 = video.read()
-            skip_frame = False
-            continue
-          else: 
-            ret, img0 = video.read()
-            skip_frame = True'''
+        # add a dimension to the image if it is a single image
+        if img.ndimension() == 3:
+          img = img.unsqueeze(0)
+
+        # Do the Inference (Prediction)
+        pred = model(img, augment= False)[0]
+
+        # Do the non-maximum suppression to remove the redundant bounding boxes
+        total_detections = 0
+        pred = non_max_suppression(pred, conf_, opt['iou-thres'], classes= classes, agnostic= False)
         
-        if ret:
-          # perform predictions
-          img = letterbox(img0, imgsz, stride=stride)[0]
-          img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
-          img = np.ascontiguousarray(img)
-          img = torch.from_numpy(img).to(device)
-          img = img.half() if half else img.float()  # uint8 to fp16/32
-          img /= 255.0  # 0 - 255 to 0.0 - 1.0
-          if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+        # Process all the predictions and draw the bounding boxes
+        for i, det in enumerate(pred):
+          s = ''
+          s += '%gx%g ' % img.shape[2:]  # print string
 
-          # Inference
-          t1 = time_synchronized()
-          pred = model(img, augment= False)[0]
+          # if there is a detection
+          if len(det):
+            # Rescale boxes from img_size (predicted image) to im0 (actual image) size and round the coordinates
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
 
-          # conf = 0.5
-          total_detections = 0
-          pred = non_max_suppression(pred, conf_, opt['iou-thres'], classes= classes, agnostic= False)
-          
-          t2 = time_synchronized()
-          for i, det in enumerate(pred):
-            s = ''
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]
-            if len(det):
-              det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+            # Consider that the frame is safe
+            unsafe = False # Flag to indicate if the frame is unsafe
 
-              unsafe = False # Flag to indicate if the frame is unsafe
+            # Write the detections to the frame
+            for c in det[:, -1].unique():
+              n = (det[:, -1] == c).sum()  # detections per class
+              total_detections += int(n)
+              c = int(c)
 
-              for c in det[:, -1].unique():
-                n = (det[:, -1] == c).sum()  # detections per class
-                total_detections += int(n)
-                c = int(c)
-
-                #we need to make sure at there is violation in atleast 5 continous frames
-                # Check if the frame is unsafe
-                if unsafe == False and (c == 0 or c == 1 or c == 3) and n > 0:
-                  unsafe = True
-                
-                s += f"{n} {names[c]}{'s' * (n > 1)}, "  # add to string
-
-              #code to send email on continous violations
-              if unsafe == True and is_email_allowed == True:
-                violation_frames += 1
-                if violation_frames >= 5 and send_next_email == True:
-                # reset the violation_frames since violation is detected
-                  violation_frames = 0
-                  # create a thread for sending email
-                  t = threading.Thread(target=violation_alert_generator, args=(img0,))
-                  t.start()
-                elif unsafe == False:
-                  # reset the number of violation_frames if current frame is safe
-                  violation_frames = 0
-
-              #get current time in hh:mm:ss format
-              current_time = time.strftime("%H:%M:%S", time.localtime())
-              detections_summary += f"\n {current_time}\n Total Detections: {total_detections}\n Detections per class: {s.split(maxsplit=1)[1]}\n###########\n"
-              #print(detections_summary)
+              #we need to make sure at there is violation in atleast 5 continous frames
+              # Check if the frame is unsafe
+              # c == 0 means no helmet, c == 1 means no jacket, c == 3 means unsafe
+              # c == 2 means safe
+              if unsafe == False and (c == 0 or c == 1 or c == 3) and n > 0:
+                unsafe = True
               
-              for *xyxy, conf, cls in reversed(det):
-                label = f'{names[int(cls)]} {conf:.2f}'
-                if label.startswith('safe'):
-                  color = (0,255,0)
-                else:
-                  color = (0,0,255)
+              # add the number of detections per class to the string
+              s += f"{n} {names[c]}{'s' * (n > 1)}, "  # add to string
 
-                plot_one_box(xyxy, img0, label=label, color=color, line_thickness=3)
+            #code to send email on five continous violations
+            if unsafe == True and is_email_allowed == True:
+              violation_frames += 1
+              if violation_frames >= 5 and send_next_email == True:
+              # reset the violation_frames since violation is detected
+                violation_frames = 0
+                # create a thread for sending email
+                t = threading.Thread(target=violation_alert_generator, args=(img0,))
+                t.start()
+              elif unsafe == False:
+                # reset the number of violation_frames if current frame is safe
+                violation_frames = 0
 
-          #fps_x = int((j+1)/(time.time() - start_time))
-          fps_x = None
-          # print(f"{j+1}/{nframes} frames processed")
-          # print(conf)
-          yield img0, fps_x, img0.shape, total_detections
-          # cv2.imshow('hello',img0)
-          # cv2.waitKey(1) & 0xFF == ord("q")
+            # Store the detections summary in a string
+            #get current time in hh:mm:ss format
+            current_time = time.strftime("%H:%M:%S", time.localtime())
+            detections_summary += f"\n {current_time}\n Total Detections: {total_detections}\n Detections per class: {s.split(maxsplit=1)[1]}\n###########\n"
+            
+            # Plot the bounding boxes on the frame
+            for *xyxy, conf, cls in reversed(det):
+              label = f'{names[int(cls)]} {conf:.2f}'
+              if label.startswith('safe'):
+                color = (0,255,0)
+              else:
+                color = (0,0,255)
 
-        else:
-          # no more frames to read
-          break
+              plot_one_box(xyxy, img0, label=label, color=color, line_thickness=3)
+
+          yield img0, total_detections
+
+    except Exception as e:
+      print(e)
     
-  '''if not is_stream:
-    # output.release()
-    video.release()'''
-# cv2.imshow("image",img0)
-# cv2.waitKey(0) & 0xFF == ord("q")
